948e5bd86447c6e519582e42505a5cbcb457e7e4
HBS02 <58508194+HBS02@users.noreply.github.com>
2021-06-13 18:07:48 +0900

    /img -> ./img

--- bg.js
+++ bg.js
@@ -1,28 +1,26 @@
 const body = document.querySelector("body");
 
 const IMG_NUMBER = 5;
- 
 
 function handleImgLoad(image) {
-    console.log("image load complete");
-    image.classList.add("bgImage");
-    body.prepend(image);
+  console.log("image load complete");
+  image.classList.add("bgImage");
+  body.prepend(image);
 }
-    
+
 function paintImage(imgNumber) {
-    const image = new Image();
-    image.src = `/images/${imgNumber + 1}.jpg`;
-    image.addEventListener("load", handleImgLoad(image));
-     
+  const image = new Image();
+  image.src = `./images/${imgNumber + 1}.jpg`;
+  image.addEventListener("load", handleImgLoad(image));
 }
 
 function genRandom() {
-    const number = Math.floor(Math.random() * IMG_NUMBER);
-    return number;
+  const number = Math.floor(Math.random() * IMG_NUMBER);
+  return number;
 }
 
 function init() {
-    const randomNumber = genRandom();
-    paintImage(randomNumber);
+  const randomNumber = genRandom();
+  paintImage(randomNumber);
 }
 init();
